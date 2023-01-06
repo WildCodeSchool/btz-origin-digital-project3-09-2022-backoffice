7f5cb2e48ef5da23b247e79b40380593ef14c216
Nicolas-CARRERE <nicolascarrere.pro@gmail.com>
2023-01-06 12:30:49 +0100

    Modification of the architecture of user routes

--- src/types/types.ts
+++ src/types/types.ts
@@ -7,3 +7,15 @@ export type TnewUser = {
   role: string;
   password: string;
 };
+
+export type TUser = {
+  id: string;
+  firstname: string;
+  lastname: string;
+  username: string;
+  email: string;
+  imageUrl: string | null;
+  role: string;
+  createdAt: Date;
+  updatedAt: Date;
+};
